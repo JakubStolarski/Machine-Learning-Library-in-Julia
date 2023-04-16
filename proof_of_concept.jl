897358dbbfd407af345653eeeae2b1e6fe0025b3
JakubStolarski <p.jakub.stolarski@gmail.com>
2023-04-16 21:08:18 +0200

    Improve implemented ML solutions and add infer function

--- proof_of_concept.jl
+++ proof_of_concept.jl
@@ -2,30 +2,31 @@
 mutable struct DenseLayer
     in_channels::Int
     out_channels::Int
-    weights::Array{Number}
+    weights::Array{Float64}
     activation::Function
+    make_flat::Bool
 end
 
 mutable struct ConvLayer
     in_channels::Int
     out_channels::Int
-    weights::Array{Number}
+    weights::Array{Float64}
     kernel_size::Int
     activation::Function
 end
 
 function conv_layer(in_channels::Int, out_channels::Int, kernel_size::Int, activation::Function=nothing)
-    weights = randn(Float32, kernel_size, kernel_size, in_channels, out_channels)
+    weights = -0.2 .+ (0.2 - (-0.2)) .* randn(Float64, kernel_size, kernel_size, in_channels, out_channels)
     return ConvLayer(in_channels::Int, out_channels::Int, weights, kernel_size, activation)
 end
 
-function dense_layer(in_channels::Int, out_channels::Int, activation::Function=nothing) 
-    weights = randn(Float32, in_channels, out_channels)
-    return DenseLayer(in_channels::Int, out_channels::Int, weights, activation)
+function dense_layer(in_channels::Int, out_channels::Int, activation::Function=nothing, make_flat::Bool=false) 
+    weights = -0.2 .+ (0.2 - (-0.2)) .* randn(Float32, in_channels, out_channels)
+    return DenseLayer(in_channels::Int, out_channels::Int, weights, activation, make_flat)
 end
 
 ## Functions
-
+using LinearAlgebra
 # Layer functions
 function conv(input, kernel, activation=nothing)
     input_height, input_width = size(input)
@@ -52,7 +53,7 @@ function conv(input, kernel, activation=nothing)
 end
 
 function fully_connect(input, weight, activation=nothing)
-    output = weight * input 
+    output = weight .* input 
     if activation != nothing
         output = activation(output)
     end
@@ -85,7 +86,7 @@ function pooling(x::Union{Matrix, Vector, Array, Int}, kernel::Int=2, stride::In
 end
 
         
-function softmax(x::Union{Matrix, Vector, Array, Int}) 
+function softmax(x::Union{Matrix, Vector, Array, Int, LinearAlgebra.Transpose{Float64, Vector{Float64}}}) 
     exp_x = exp.(x)
     return exp_x ./ sum(exp_x)
 end
@@ -93,15 +94,25 @@ end
         
 function int_to_array(x::Int, length::Int)
     output = zeros(length)
-    output[x] = 1
+    output[x+1] = 1
     return output
 end
         
 # Loss function        
-function cross_entropy_loss(pred::Union{Matrix, Vector, Array, Int}, dest::Int)
-    num_of_classes = size(pred,1)
-    label = int_to_array(d, num_of_classes)
-    loss = -sum(dest .* log.(pred))/N
+function cross_entropy_loss(pred::Union{Matrix, Vector, Array}, dest::Union{Int,Variable,Constant})
+    num_of_classes = Constant(size(pred)[2])
+    label = Constant(int_to_array(dest, num_of_classes))
+    loss = -sum(dest .* log.(pred))/num_of_classes
+    return loss
+end
+
+function cross_entropy_loss(pred::Union{Matrix, Vector, Array}, dest::Vector{Number})
+    num_of_classes = size(pred)[2]
+    loss = []
+    for id in 1:size(dest)
+        label = int_to_array(dest[id], num_of_classes)
+        push!(loss, -sum(label .* log.(pred[id]))/num_of_classes)
+    end
     return loss
 end
         
@@ -122,20 +133,193 @@ end
 function create_net()
     conv1 = conv_layer(1, 6, 5, relu)
     conv2 = conv_layer(6, 16, 5, relu)
-    fc1 = dense_layer(16*5*5, 120, relu)
+    fc1 = dense_layer(16*5*5, 120, relu, true)
     fc2 = dense_layer(120, 84, relu)
     fc3 = dense_layer(84, 10, relu)
-    sequence = [conv, conv, fully_connect, fully_connect, fully_connect]
-    return ConvNet(conv1, conv2, fc1, fc2, fc3)
+    sequence = [conv, conv, Iterators.flatten, fully_connect, fully_connect, fully_connect]
+    return ConvNet(conv1, conv2, fc1, fc2, fc3, sequence)
 end
 
-x = [1.0 2.0 3.0 -4.0;
-     5.0 -6.0 7.0 8.0;
-     9.0 -10.0 -11.0 12.0;
-     -13.0 14.0 15.0 16.0]
-kernel = 2
-stride = 1
-out = pooling(x)
+using MLDatasets, Images, ImageMagick, Shuffle
+    
+function load_train_data(shape::Tuple=(32,32), batch_size::Int=64)
+    images, labels = MNIST.traindata(Float64)
+    resized_images = []
+    for i in 1:size(images)[3]
+        img = images[:,:,i]
+        resized_img = imresize(img, (32, 32))
+        push!(resized_images, resized_img)
+    end
+    
+    num_samples = length(resized_images)
+    num_batches = div(num_samples, batch_size)
+    batches = []
+    indices = shuffle(1:num_samples)
+    
+    for i in 1:num_batches
+        batch_indices = indices[(i-1)*batch_size + 1:i*batch_size]
+        batch_images = [resized_images[j] for j in batch_indices]
+        batch_labels = [labels[j] for j in batch_indices]
+        push!(batches, (batch_images, batch_labels))
+    end
+
+    return batches
+end
+    
+    
+function load_test_data(shape::Tuple=(32,32), batch_size::Int=64)
+    images, labels = MNIST.testdata(Float64)
+    resized_images = []
+    for i in 1:size(images)[3]
+        img = images[:,:,i]
+        resized_img = imresize(img, (32, 32))
+        push!(resized_images, resized_img)
+    end
+    
+    num_samples = length(images_resized)
+    num_batches = div(num_samples, batch_size)
+    batches = []
+    indices = shuffle(1:num_samples)
+    
+    for i in 1:num_batches
+        batch_indices = indices[(i-1)*batch_size + 1:i*batch_size]
+        batch_images = [resized_images[j] for j in batch_indices]
+        batch_labels = [labels[k] for k in batch_indices]
+        push!(batches, (batch_images, batch_labels))
+    end
+    
+    return batches
+end
+
+
+function infer(model::ConvNet, input::Union{Matrix,Vector})
+    output =[]
+    push!(output,input)
+    for layer in fieldnames(typeof(model))
+        if isa(getfield(net,layer), ConvLayer)
+            field = getfield(net,layer)
+            println(size(field.weights))
+            new_output = []
+                for kernel_set in 1:field.out_channels
+                    step_output = []
+                    for in_image in 1:field.in_channels
+                        if step_output == []
+                        step_output = pooling(field.activation(conv(output[in_image],field.weights[:,:,in_image,kernel_set])),2)
+                        else
+                        step_output .+ pooling(field.activation(conv(output[in_image],field.weights[:,:,in_image,kernel_set])),2)
+                        end
+                    end
+                    push!(new_output,step_output)
+                end
+
+
+            output = new_output
+        end
+                
+        if isa(getfield(model,layer), DenseLayer)
+            field = getfield(model,layer)
+            if field.make_flat == true
+                output = transpose(collect(Iterators.flatten((output))))
+            end
+            output = output * field.weights  # todo MAKE FC FUNCTION DO THAT, DON"T FORGET ABOUT ACTIVATION FUNCTION
+        end
+    end
+    return output
+end
+                             
+    
+function training(num_of_epochs::Int, model::ConvNet, training_data, training_labels, criterion::Function, optimizer::Function)
+    for epoch in 1:num_of_epochs
+        for training_batch in training_data
+            image = training_batch[1][1]
+            label = training_batch[1][2]
+            
+            outputs = infer(model,image)
+            loss = criterion(outputs, label)
+        end
+    end
+end
+
+
+      
+
+
+net = create_net()
+learning_rate = 0.016
+num_of_epochs = 10
+        
+train_loader = load_train_data()
+u = train_loader[1][1][1]
+criterion = cross_entropy_loss
+pred = infer(net,u)
+pred = softmax(pred)
+d = train_loader[1][2][1]
+cross_entropy_loss(pred,d)
+
+
+# Dummy function
+function infer(model::ConvNet, input::Union{Matrix,Vector})
+    final_output = []
+    for i in 1:size(input)[1]
+        image_output = input[i]
+        for layer in fieldnames(typeof(model))
+            if isa(getfield(model,layer), ConvLayer)
+                field = getfield(model,layer)
+                new_image_output = []
+                for kernel_set in 1:field.out_channels
+                    step_output = []
+                    for in_image in 1:field.in_channels
+                        if step_output == []
+                        step_output = pooling(field.activation(conv(image_output[in_image],field.weights[:,:,in_image,kernel_set])),2)
+                        else
+                        step_output .+ pooling(field.activation(conv(image_output[in_image],field.weights[:,:,in_image,kernel_set])),2)
+                    end
+                    push!(new_output,step_output)
+                end
+
+                image_output = new_output
+            end
+
+            if isa(getfield(model,layer), DenseLayer)
+                field = getfield(model,layer)
+                if field.make_flat == true
+                    image_output = collect(Iterators.flatten((output)))
+                end
+                new_output = []
+                for out_neuron in 1:field.out_channels
+                    push!(new_output, fully_connect(image_output, field.weights[:,out_neuron], field.activation))      
+                end
+
+                image_output = new_output
+            end
+        end
+        ret
+end
+
+using Plots
+
+"""
+    display_image(image::Array{Float64,2})
+Display a 2D array as an image.
+
+# Arguments
+- `image::Array{Float64,2}`: The 2D array representing the image.
+
+"""
+function display_image(image::Array{Float64,2})
+    plot(
+        heatmap(image,
+                aspect_ratio=:equal,
+                frame=:none,
+                c=:grays,
+                colorbar=false,
+                legend=false),
+        ticks=nothing,
+        border=:none,
+        axis=:off
+    )
+end
+
 
 abstract type GraphNode end
 abstract type Operator <: GraphNode end
@@ -304,116 +488,26 @@ backward(::BroadcastedOperator{typeof(max)}, x, y, g) = let
     tuple(Jx' * g, Jy' * g)
 end
 
-Base.Broadcast.broadcated(maximum, x::GraphNode) = BroadcatedOperator(maximum,x)
-forward(::BroadcastedOperator{typeof(maximum)}, x) = return maximum(x)
-backward(::BroadcastedOperator{typeof(maximum)},x, g) = 
+# Base.Broadcast.broadcasted(maximum, x::GraphNode) = BroadcatedOperator(maximum,x)
+# forward(::BroadcastedOperator{typeof(maximum)}, x) = return maximum(x)
+# backward(::BroadcastedOperator{typeof(maximum)},x, g) = 
 
-using MLDatasets, Images, ImageMagick
-
-function load_train_data(shape::Tuple=(32,32), batch_size::Int=64)
-    images, labels = MNIST.traindata()
-    images_resized = [imresize(image, shape) for image in images]
-    
-    num_samples = length(images_resized)
-    num_batches = div(num_samples, batch_size)
-    batches = []
-    indices = shuffle(2:num_samples)
-    
-    for id in indices
-        batch_indices = indices[(i-1)*batch_size + 1:i*batch_size]
-        batch_images = [images[j] for j in batch_indices]
-        batch_labels = [labels[j] for j in batch_indices]
-        push!(batches, (batch_images, batch_labels))
-    end
-    
-    return batches
+x = Variable(randn(1,400),name="x")
+y = Variable(randn(1,10),name="y")
+w1 = Variable(net.conv1.weights, name="w1")
+w2 = Variable(net.conv2.weights, name="w2")
+w3 = Variable(net.fc1.weights, name="w3")
+w4 = Variable(net.fc2.weights, name="w4")
+w5 = Variable(net.fc3.weights, name="w5")
+function testing_net()
+    x1 = relu(x*w3)
+    x2 = relu(x1*w4)
+    x3 = x2*w5
+    x4 = softmax(x3)
+    x5 = cross_entropy_loss(x4,y)
+    return topological_sort(loss)
 end
-    
-    
-function load_test_data(shape::Tuple=(32,32), batch_size::Int=64)
-    images, labels = MNIST.testdata()
-    images_resized = [imresize(image, shape) for image in images]
-    
-    num_samples = length(images_resized)
-    num_batches = div(num_samples, batch_size)
-    batches = []
-    indices = shuffle(2:num_samples)
-    
-    for id in indices
-        batch_indices = indices[(i-1)*batch_size + 1:i*batch_size]
-        batch_images = [images[j] for j in batch_indices]
-        batch_labels = [labels[j] for j in batch_indices]
-        push!(batches, (batch_images, batch_labels))
-    end
-    
-    return batches
-end
-
-function infer(model::ConvNet, input::Matix)
-    output = []
-    for layer in fieldnames(typeof(ConvNet)):
-        if typeof(getfield(model,layer)) isa ConvNet
-            for i in 1:model.layer.out_channels
-                push!(output, pooling(conv(output,model.layer.kernel_size,model.layer.weights),2))
-                            
-    x = conv2d(x, model.conv1.weight) 
-    x = model.conv1.activation.(x)
-    x = maxpool(x, (2, 2))
-    x = conv2d(x, model.conv2.weight)
-    x = model.conv2.activation.(x)
-    x = maxpool(x, (2, 2))
-    x = reshape(x, :, size(x, 4))
-    x = model.fc1.activation.(model.fc1.weight' * x .+ model.fc1.bias)
-    x = model.fc2.activation.(model.fc2.weight' * x .+ model.fc2.bias)
-    return x
-            
-    conv1 = conv_layer(1,6,5,relu)
-    conv2 = conv_layer(6,16,5,relu)
-    fc1 = dense_layer(16*5*5, 120,relu)
-    fc2 = dense_layer(120, 84,relu)
-    fc3 = dense_layer(84, 10,relu)
-            
-            
-function training(num_of_epochs::Int, model::ConvNet, training_data, training_labels, criterion::Function, optimizer::Function)
-    for epoch in 1:num_of_epochs
-        for training_batch in training_data
-            image = training_batch[1]
-            label = training_batch[2]
-                    
-            out
-        
-end
-
-net = create_net()
-learning_rate = 0.016
-num_of_epochs = 10
-train_loader = load_train_data()
-test_loader = load_test_data()
-
-
-
-net = create_net()
-net.conv1.activation([-2])
-
-a = randn(Float32, 5, 5, 1, 6)
-b = randn(Float32, 32, 32)
-c = []
-for i in 1:6
-    push!(c, pooling(conv(b, a[:,:,1,i],relu),2))
-end
-d = randn(Float32, 5, 5, 6, 16)
-e = []
-size(c)
-# d[:,:,1,1]
-    
-for (j,c_layer) in enumerate(c)
-    for k in 1:16
-        push!(e, pooling(conv(c_layer, d[:,:,j,k],relu),2))
-    end
-end
-(u,v) = enumerate(a)
-u
-
+graph = testing_net()
 
 
 
